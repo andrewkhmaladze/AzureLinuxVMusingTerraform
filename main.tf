9e11184248ee097b6d02c924a6a120b21355e568
andrew <andrew.khmaladze@gmail.com>
2025-11-19 07:16:49 -0500

    updated files that created a resource group, a vm , and many other things on Azure

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-tf-vm-lab"
+  name     = "rg-tf-vm-lab-andrew"  # Add your name to make it unique
   location = var.location
 }
- 
+
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tf-vnet"
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
- 
+
 # Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "tf-subnet"
@@ -19,13 +19,13 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
- 
+
 # NSG
 resource "azurerm_network_security_group" "nsg" {
   name                = "tf-nsg"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
- 
+
   security_rule {
     name                       = "SSH"
     priority                   = 100
@@ -37,7 +37,7 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
- 
+
   security_rule {
     name                       = "HTTPS"
     priority                   = 110
@@ -50,22 +50,22 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
- 
+
 # Public IP
 resource "azurerm_public_ip" "pip" {
   name                = "tf-public-ip"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Basic"
+  sku                 = "Standard"  # Change from "Basic" to "Standard"
 }
- 
+
 # NIC
 resource "azurerm_network_interface" "nic" {
   name                = "tf-nic"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
- 
+
   ip_configuration {
     name                          = "primary"
     subnet_id                     = azurerm_subnet.subnet.id
@@ -73,13 +73,13 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
- 
+
 # Associate NSG with NIC
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
- 
+
 # VM
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "tf-ubuntu"
@@ -90,17 +90,17 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
- 
+
   admin_ssh_key {
     username   = var.vm_admin_user
     public_key = file(var.public_key_path)
   }
- 
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
- 
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "azurerm" {
   features {}
+  
+  subscription_id = "60f30377-5258-481a-b733-7b479de6a701"
+  tenant_id       = "bdc7e434-9144-41a1-883f-75c142492e35"
 }
